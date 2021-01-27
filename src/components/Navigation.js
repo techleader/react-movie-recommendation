fe46a926715aeb0de6878b79bcb9f16584178d1c
jawahar <jawahar.career@gmail.com>
2021-01-28 01:31:08 +0530

    logo added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { Navbar, Nav, NavItem, NavbarBrand, Button, Row, Col } from 'reactstrap';
-
+import image from '../images/logo.png'
 
 class Navigation extends Component {
 
@@ -12,7 +12,16 @@ class Navigation extends Component {
       return (
          <div>
             <Navbar color="dark">
-               <NavbarBrand ><h4 className="text-danger   ">Prime Video </h4></NavbarBrand>
+               <NavbarBrand >
+                  <Row>
+                  <Col sm={2}></Col>
+                     <Col sm={2}>   <img src={image} width="50%" height="100%" ></img></Col>
+                    
+                     <Col sm={7}></Col>
+                  </Row>
+
+
+               </NavbarBrand>
                <Nav >
                   <NavItem>
                      <NavLink to="/"><Button color="info">Home</Button> </NavLink>
@@ -27,7 +36,7 @@ class Navigation extends Component {
                         //className="searchCampaign"
                         />
                      </NavLink> */}
-                     <NavLink to="/Settings"><Button color="info"> Settings</Button> </NavLink>
+                     <NavLink to="/Settings"><Button color="info"> Sign in</Button> </NavLink>
                   </NavItem>
                </Nav>
             </Navbar>
@@ -37,9 +46,9 @@ class Navigation extends Component {
    }
 }
 
-const mapStateToProps = ( state ) => {
-   console.log( "InMaptoProps" );
-   console.log( state );
+const mapStateToProps = (state) => {
+   console.log("InMaptoProps");
+   console.log(state);
    return {
       generalVal: state.generalVal,
       businessVal: state.businessVal,
@@ -49,4 +58,4 @@ const mapStateToProps = ( state ) => {
 
 };
 
-export default connect( mapStateToProps, null )( Navigation );
+export default connect(mapStateToProps, null)(Navigation);
